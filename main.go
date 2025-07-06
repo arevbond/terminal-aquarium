8e0a4d54b1635d07f952f3e1a980f75d92a8282e
Nikita Bondarev <bondarev.n17@yandex.ru>
2025-07-06 19:39:15 +0300

    add swimming fish

--- main.go
+++ main.go
@@ -1,27 +1,81 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gdamore/tcell"
 	"log"
 )
 
-var fish = "><_>"
+//var fish = "><_>"
+
+var bigFish = `
+o           //        +
+ o        //////    ++
+  .    @))))))))))+++
+   .}:<<)))))))))))++
+       <<\\)))))) +++
+          \\   \\   ++
+                      +`
+
+var bigFish2 = `
+     |\    o
+    |  \    o
+|\ /    .\ o
+| |       (
+|/ \     /
+    |  /
+     |/
+`
+
+var smallFishSlice = []string{
+	"  _ ",
+	"><_>",
+}
+
+var fishForward = []string{
+	"     |\\    o",
+	"    |  \\    o",
+	"|\\ /    .\\ o",
+	"| |       (",
+	"|/ \\     /",
+	"    |  /",
+	"     |/",
+}
 
 func main() {
-	backgroundStyle := tcell.StyleDefault.Foreground(tcell.ColorRed).Background(tcell.ColorBlue)
+	backgroundStyle := tcell.StyleDefault.Foreground(tcell.ColorRed).Background(tcell.ColorWhite)
 
 	s, err := tcell.NewScreen()
 	if err != nil {
 		log.Fatalf("%+v", err)
 	}
 
+	x, y := s.Size()
+	fmt.Printf("w: %d, h: %d\n", x, y)
+
 	if err = s.Init(); err != nil {
 		log.Fatalf("%+v", err)
 	}
 
 	s.SetStyle(backgroundStyle)
+	if err = s.Beep(); err != nil {
+		fmt.Println("err", err)
+	}
 
 	var start int
+
+	quit := func() {
+		maybePanic := recover()
+		s.Fini()
+		if maybePanic != nil {
+			panic(maybePanic)
+		}
+	}
+	defer quit()
+
+	fish := fishForward
+
+	isForward := true
 
 	for {
 		s.Show()
@@ -33,15 +87,43 @@ func main() {
 			if ev.Key() == tcell.KeyCtrlC {
 				return
 			}
+			s.Clear()
 
-			if ev.Key() == tcell.KeyCtrlA {
-				s.Clear()
-				drawText(s, start, start+len(fish)+1, 1, len(fish)+1, tcell.StyleDefault, fish)
+			drawFish(s, start, tcell.StyleDefault.Foreground(tcell.ColorBlue), fish)
+
+			if isForward {
 				start++
+			} else {
+				start--
+			}
+
+			if start == 50 {
+				isForward = false
+				fish = reverse(fish)
 			}
 		}
-
 	}
+}
+
+func drawFish(s tcell.Screen, startX int, style tcell.Style, fish []string) {
+	for col := 0; col < len(fish); col++ {
+		for row := 0; row < len(fish[col]); row++ {
+			s.SetContent(startX+row, col, rune(fish[col][row]), nil, style)
+		}
+	}
+}
+
+func reverse(fish []string) []string {
+	result := make([]string, 0, len(fish))
+	for _, str := range fish {
+		newStr := ""
+		for r := len(str) - 1; r >= 0; r-- {
+			newStr += string(str[r])
+		}
+		result = append(result, newStr)
+	}
+
+	return result
 }
 
 func drawText(s tcell.Screen, x1, x2, y1, y2 int, style tcell.Style, text string) {
